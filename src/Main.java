76dbe0401dd78bfb17b8d919d95b8509b69cd35c
Meaghan Bailey <baile3mr@mail.uc.edu>
2026-09-17 15:28:25 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+    int numToExamine = 2;
+
+    IO.println("If mod 2 is ) then Even. Else Mod 1 is Odd");
+    IO.println(numToExamine % 2);
+}
